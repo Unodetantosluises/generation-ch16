cea278cf735a51c0ce472b8e2003b0529195cb93
Luis Diaz <luisantoniodiaz97@gmail.com>
2022-08-02 22:37:41 -0500

    Practica de JavaScript para la Web

--- 3 JavaScript/js-01/assets/casteo.js
+++ 3 JavaScript/js-01/assets/casteo.js
@@ -9,21 +9,20 @@ let num2 = "10";
 
 console.log(num + num2);
 
-let num3 = "56";
-console.log(Number(num3));
+let num3 = Number("56");
 console.log(num3);
 
 /* String() */
 // Convierte nuemor o booleanos a cadenas
-let num4 = String(4);
+let num4 = 4;
 console.log(num4);
-console.log(typeof (num4));
+console.log(String (num4));
 
 // Boolean
 // conviette numeros y cadenas a booleanos
 // 0, "", null, - false
 // todos los demas numeros ls convierte a true
 
-let num5 = Boolean(-1);
+let num5 = Boolean(0);
 console.log(num5);
 console.log(typeof num5);
