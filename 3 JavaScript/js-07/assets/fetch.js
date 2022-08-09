f9ffc87bee94ddb8d7e6260ee003003dca5ef152
Luis Diaz <luisantoniodiaz97@gmail.com>
2022-08-09 12:15:02 -0500

    JavaScript Asincrono, Promesas y Fetch

---
+++ 3 JavaScript/js-07/assets/fetch.js
@@ -0,0 +1,32 @@
+const url = "https://dog.ceo/api/breeds/image/random"
+
+// Fetch es una funcion  que te regresa una promesa, cponsultando datos de internet
+fetch(url).then((respuesta)=>{
+    console.log(respuesta)
+})
+
+// Obtener la informacion de la peticion
+// Forma 1
+fetch(url)
+.then((resp)=> resp.json())
+.then((datos)=>{
+
+    console.log(datos);
+
+}).catch((error)=>{
+    console.log(error);
+})
+
+// Forma 2
+async function obtenerPerritoAleatorio(){
+    try{
+    const respuesta = await fetch(url);
+    const datos = await respuesta.json()
+
+    console.log(datos);
+}catch(error){
+    console.log(error)
+}
+}
+
+obtenerPerritoAleatorio()
