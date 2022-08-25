9d43dfbf941f418d93cfb0e3b3ecc258f2c42387
Luis Diaz <luisantoniodiaz97@gmail.com>
2022-08-25 11:09:38 -0500

    Ejercicio en Equipo y POO en Java

---
+++ 4 JDK  Java/pooJava/src/miEjemploJava/ejemploPersona.java
@@ -0,0 +1,12 @@
+package miEjemploJava;
+
+public class ejemploPersona {
+    Persona p = new Persona();
+       p.setNombre("Dora");
+       p.setEdad(32);
+       p.setNss(14689);
+        System.out.println("el valor de la variable de instancia p.nombre -> "+p.getNombre());
+        System.out.println("el valor de la variable de instancia p.edad -> "+p.getEdad());
+        System.out.println("el valor de la variable de instancia p.nss -> "+p.getNss());
+
+}
--- 4 JDK  Java/pooJava/src/miSaludo/ejemploSaludo.java
+++ 4 JDK  Java/pooJava/src/miSaludo/ejemploSaludo.java
@@ -1,3 +1,7 @@
+package miSaludo;
+
+import miSaludo.Saludo;
+
 public class ejemploSaludo {
     public static void main(String[] args) {
         Saludo objSaludo;//Declara el objeto
@@ -8,5 +12,12 @@ public class ejemploSaludo {
 
         System.out.println(objSaludo.saludar0());
         System.out.println("Este es otro objeto: ->" + objSaludo0.saludar0());
+        objSaludo.textoS = "Hola";
+        System.out.println("objSaludo = " + objSaludo0);
+
+        objSaludo0.a = 4;
+        System.out.println("objSaludo0 = " + objSaludo0);
+
+
     }
 }
