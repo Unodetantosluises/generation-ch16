8bb81de6f3b1fd9f54f681e10715ee53cc2e6aa8
Luis Diaz <luisantoniodiaz97@gmail.com>
2022-08-28 09:42:38 -0500

    Ejercicio sobre la Herencia.

---
+++ 4 JDK  Java/pooJavaAvanzado/src/com/ladm/herencia/basePadre.java
@@ -0,0 +1,40 @@
+package com.ladm.herencia;
+
+public class basePadre {
+    protected int a;
+    private double b;
+    private String c;
+    public basePadre() {
+
+    }
+    public void visualizarA(){
+        System.out.println("Metodo visualizarA de la clase basePadre.");
+
+    }
+    public void visualizarABC(){
+        System.out.println("Metodo visualizarABC de la clase basePadre.");
+    }
+    public int getA(){
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public String getC() {
+        return c;
+    }
+
+    public void setC(String c) {
+        this.c = c;
+    }
+}
---
+++ 4 JDK  Java/pooJavaAvanzado/src/com/ladm/ladm/principal/ejemploHerencia.java
@@ -0,0 +1,22 @@
+package com.ladm.ladm.principal;
+
+import com.ladm.herencia.basePadre;
+import com.ladm.herencia.hija;
+import com.ladm.herencia.hijo;
+
+public class ejemploHerencia {
+    public static void main(String[] args) {
+        //estoy creando un objeto "h" de la clase hija
+        hija h = new hija();
+        h.visualizarA();
+        hijo ho = new hijo();
+        System.out.println("Llama al metodo visualizarABC,");
+        ho.visualizarABC();
+
+        basePadre obj = new basePadre();
+        obj.setA(777);
+
+        System.out.println("Atributo de Clase base Padre -> " + h.getA());
+
+    }
+}
