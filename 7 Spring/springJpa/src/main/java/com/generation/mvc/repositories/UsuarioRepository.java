4a25475308188d5328d67ba16ae60376223a9c38
Luis Diaz <luisantoniodiaz97@gmail.com>
2022-09-15 11:26:13 -0500

    CRUD usando Spring y Postman

---
+++ 7 Spring/springJpa/src/main/java/com/generation/mvc/repositories/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package com.generation.mvc.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends CrudRepository<UsuarioModel, Long>{
+
+}
